59fedf7462b19460528580115303a1634aa9f1e6
sebasmos <ulsordonez@unicauca.edu.co>
2022-10-14 11:53:08 -0400

    3 channel models#

---
+++ test.sh
@@ -0,0 +1,4 @@
+echo "python test.py --dataroot /n/pfister_lab2/Lab/scajas/DATASETS/DATASET_pix2pix/test --model pix2pix --name vcg_augmented_horiz_v3_500_epochs  --results_dir tiff --dataset_mode vcg  --direction BtoA  "
+python test.py --dataroot /n/pfister_lab2/Lab/scajas/DATASETS/DATASET_pix2pix/test --model pix2pix --name vcg_augmented_horiz_v3_500_epochs  --results_dir tiff --dataset_mode vcg  --direction BtoA  # --output_nc 19 #--input_nc 3  #--direction BtoA # --netG resnet_9blocks #
+
+#python test.py --dataroot /n/pfister_lab2/Lab/scajas/DATASETS/DATASET_pix2pix/ --direction BtoA --model pix2pix --name vcg_changing_deconv_by_upsam --use_wandb --results_dir borrar1
