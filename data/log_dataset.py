5d8019cd5b761b6d4e9ed9b2f7f389b85fbf0aad
sebasmos <ulsordonez@unicauca.edu.co>
2023-07-04 13:32:47 -0400

    new multi proc 3

--- data/log_dataset.py
+++ data/log_dataset.py
@@ -36,12 +36,11 @@ class logDataset(BaseDataset):
         targets_path = os.path.join(self.root, "img_if", self.targets[index])
         img = tifffile.imread(img_path)
         target = tifffile.imread(targets_path)
-        target = skimage.util.img_as_float32(target)#**ADDED**
+        target = skimage.util.img_as_float32(target)
         img = np.moveaxis(img, 0, 2)
-        # Normalize images
+
         CHANNELS = range(19)
         log_img = np.log10(target, where=(target != 0))
-        #print("3: (max of log_img in float32) ", log_img.max(), log_img.shape)
             
         lower_cutoff_log = np.percentile(log_img.ravel(), 0.17)
         upper_cutoff_log = np.percentile(log_img.ravel(), 99.99)
@@ -49,16 +48,10 @@ class logDataset(BaseDataset):
                 (((1-0)*(log_img.ravel()-lower_cutoff_log)) /
                 (upper_cutoff_log-lower_cutoff_log)
                 ) + 0).reshape(log_img.shape)
-        #print("4: (max of rescaled_log_img in float32) ", rescaled_log_img.max(), rescaled_log_img.shape)
             
         target = np.clip(a=rescaled_log_img, a_min=0, a_max=1)
         target = np.moveaxis(target, 0, 2)
-        #print("5: (max of target in float32) ", target.max(), target.shape)
-        
         img, target = self.transforms_he(img), self.transforms_if(target)
-        #print("6: (max of target in float32) ", target.max(), target.shape)
-        #print(f"TARGET SIZE IN DATALOADER: ", target.shape, img.shape)
-        #return {'A': img, 'B': target, 'A_paths': img_path, 'B_paths': targets_path}
         return {'A': target, 'B': img, 'A_paths': targets_path, 'B_paths': img_path}
     
     def __len__(self):
@@ -69,12 +62,9 @@ def get_transform(train, size=256, HE_IF = "he"):
     transforms.append(T.ToTensor())
 
     if train==1:
-        print("training data!!")
         transforms.append(T.Resize((size,size)))
-        # Adding transformation to both inputs
         transforms.append(T.RandomHorizontalFlip(0.5))
     else:
-        print("testing data!!")
         transforms.append(T.Resize((size,size)))
         
     return T.Compose(transforms)
